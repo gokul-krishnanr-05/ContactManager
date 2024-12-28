73614e0883f18faefa485de9ecab3d306885d0b0
gokul <gokulkrishnanr123456@gmail.com>
2024-12-28 23:26:41 +0530

    create page to add contact and tested for functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';  // Importing necessary components from react-router-dom
-import Home from './Components/Home';  // Importing Home component
-import Addcontact from './Components/Addcontact';  // Importing AddContact component
+import { Routes, Route } from 'react-router-dom';  
+import Home from './Components/Home';  
+import Addcontact from './Components/Addcontact';  
 
 const App = () => {
   return (
     <div>
       <Routes>
-        {/* Route for Home page */}
+      
         <Route path="/" element={<Home />} />
-        
-        {/* Route for Add Contact page */}
-        <Route path="/add" element={<Addcontact />} />
+          <Route path="/add" element={<Addcontact />} />
       </Routes>
     </div>
   );

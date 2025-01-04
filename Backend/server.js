02db6ffae8965787711435f2120d603217a318d9
gokul <gokulkrishnanr123456@gmail.com>
2025-01-04 12:31:18 +0530

    backend cors updated

--- Backend/server.js
+++ Backend/server.js
@@ -3,25 +3,25 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
-const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
+
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: [
+    "http://localhost:3000",
+    "https://contactmanager-93.netlify.app"
+  ],
 };
-const corsOptions1 = {
-  origin: "https://contactmanager-93.netlify.app/",
-};
-app.use(cors(corsOptions1));
+
+app.use(cors(corsOptions));
 
 mongoose
   .connect(process.env.DB_URI)
   .then(() => console.log("Connected to Database"))
-  .catch((err) => console.error(err)); 
+  .catch((err) => console.error(err));
 
 const ContactRouter = require("../Backend/Routes/ContactRoutes");
 app.use("/api", ContactRouter);
 
-
-
 app.listen(PORT, () => {
-  console.log(` listening on http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
